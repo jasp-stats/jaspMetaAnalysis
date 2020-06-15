0114f344e8b2c1fa7acca047c9637fa9d9cbe877
FBartos <38475991+FBartos@users.noreply.github.com>
2020-06-15 17:04:23 +0200

    removing trash + re-alligning qml input

--- tests/testthat.R
+++ tests/testthat.R
@@ -11,8 +11,3 @@ result <- as.data.frame(result)
 
 if (sum(result$failed) > 0 || sum(result$error) > 0)
   quit(save = "no", status = 1)
-
-
-MA_test <- read.delim("D:/Desktop/MA_test.txt")
-options <- readRDS(file = "D:/Projects/jasp/jasp-R-debug/options.RDS")
-results <- jasptools::run("RobustBayesianMetaAnalysis", "debug.csv", options, makeTests = F)
