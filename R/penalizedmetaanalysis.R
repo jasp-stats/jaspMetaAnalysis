eed51852deff41505e241255989674cf990dc92b
Frantisek Bartos <f.bartos96@gmail.com>
2022-05-01 12:21:29 +0200

    full of debug stuff

---
+++ R/penalizedmetaanalysis.R
@@ -0,0 +1,474 @@
+#
+# Copyright (C) 2013-2018 University of Amsterdam
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# TODO: make tau2 or tau2_w and tau2_b always available for diagnostics
+PenalizedMetaAnalysis <- function(jaspResults, dataset = NULL, options, ...) {
+
+  library(pema)
+  saveRDS(options, file = "C:/JASP/jaspMetaAnalysis/options.RDS")
+
+
+  if (.pemaCheckReady(options)) {
+    # get the data
+    dataset <- .pemaGetData(dataset, options)
+    dataset <- .pemaCheckData(jaspResults, dataset, options)
+
+    # fit the models
+    .pemaFit(jaspResults, dataset, options)
+  }
+
+  if (options[["estimatesCoefficients"]])
+    .pemaSummaryTable(jaspResults, options)
+
+  if (options[["estimatesTau"]])
+    .pemaSummaryTableTau(jaspResults, options)
+
+  if (length(options[["diagnosticsVariable1"]]) > 0)
+    .pemaDiagnostics(jaspResults, options)
+
+  return()
+}
+
+.pemaDependencies <- c(
+  "effectSize", "standardError", "method", "studyLabels", "covariates", "factors", "clustering", "components", "modelTerms", "includeConstant",
+  "priorHsDf", "priorHsScale", "priorLassoDf", "priorLassoDfGlobal", "priorLassoDfSlab", "priorLassoScaleGlobal", "priorLassoScaleSlab",
+  "mcmcWarmup", "mcmcIter", "mcmcChains", "mcmcDelta", "mcmcTreedepth"
+)
+
+# check and load functions
+.pemaCheckReady                <- function(options) {
+  return(options[["effectSize"]] != "" && options[["standardError"]] != "")
+}
+.pemaGetData                   <- function(dataset, options) {
+
+  if (!is.null(dataset))
+    return(dataset)
+  else {
+    effsizeName <- unlist(options$effectSize)
+    stderrName  <- unlist(options$standardError)
+    covarNames  <- if (length(options$covariates)  > 0) unlist(options$covariates)
+    factNames   <- if (length(options$factors)     > 0) unlist(options$factors)
+    clustNames  <- if (length(options$clustering)  > 0) unlist(options$clustering)
+    studyLabels <- if (length(options$studyLabels) > 0) unlist(options$studyLabels)
+
+    numeric.variables <- Filter(function(s) s != "", c(effsizeName, covarNames, stderrName))
+    factor.variables  <- Filter(function(s) s != "", c(factNames, studyLabels, clustNames))
+
+    dataset <- .readDataSetToEnd(columns.as.factor   = factor.variables,
+                                 columns.as.numeric  = numeric.variables,
+                                 exclude.na.listwise = numeric.variables)
+
+    # precompute variance
+    dataset$JASP_computed_variance__ <- dataset[,stderrName]^2
+
+    return(dataset)
+  }
+}
+.pemaCheckData                 <- function(jaspResults, dataset, options) {
+
+  datasetOld <- dataset
+  dataset    <- na.omit(dataset)
+
+  # store the number of missing values
+  if (nrow(datasetOld) > nrow(dataset)) {
+    if (!is.null(jaspResults[["nOmitted"]])) {
+      nOmitted <- jaspResults[["nOmitted"]]
+    } else {
+      nOmitted <- createJaspState()
+      nOmitted$dependOn(.pemaDependencies)
+      jaspResults[["nOmitted"]] <- nOmitted
+    }
+    nOmitted$object <- nrow(datasetOld) - nrow(dataset)
+  }
+
+  .hasErrors(dataset               = dataset,
+             type                  = c("infinity", "observations"),
+             observations.amount   = "< 2",
+             exitAnalysisIfErrors  = TRUE)
+
+  if (options[["standardError"]] != "")
+    .hasErrors(dataset               = dataset,
+               type                  = c("negativeValues"),
+               negativeValues.target = options[["inputSE"]],
+               exitAnalysisIfErrors  = TRUE)
+
+  return(dataset)
+}
+.pemaFormula                   <- function(options) {
+
+  if (length(options[["modelTerms"]]) == 0 && options[["includeConstant"]])
+    formula <- paste(options[["effectSize"]], "~", "1")
+  else if (length(options[["modelTerms"]]) > 0 && options[["includeConstant"]])
+    formula <- paste(options[["effectSize"]], "~", paste0(sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":")), collapse = "+"))
+  else if (length(options[["modelTerms"]]) > 0 && !options[["includeConstant"]])
+    formula <- paste(options[["effectSize"]], "~", paste0(sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":")), collapse = "+"), "-1")
+  else
+    .quitAnalysis(gettext("The model should contain at least one predictor or an intercept."))
+
+
+  return(as.formula(formula, env = parent.frame(1)))
+}
+.pemaVariableNames             <- function(varName, terms) {
+
+  if (varName == "Intercept")
+    return(gettext("Intercept"))
+
+  for (vn in terms) {
+    inf <- regexpr(vn, varName, fixed = TRUE)
+
+    if (inf[1] != -1) {
+      varName <- paste0(
+        substr(varName, 0, inf[1] - 1),
+        substr(varName, inf[1], inf[1] + attr(inf, "match.length") - 1),
+        " (",
+        substr(varName, inf[1] + attr(inf, "match.length"), nchar(varName))
+      )
+    }
+
+  }
+
+  varName <- gsub(":", paste0(")", jaspBase::interactionSymbol), varName, fixed = TRUE)
+  varName <- paste0(varName, ")")
+  varName <- gsub(" ()", "", varName, fixed = TRUE)
+
+  return(varName)
+}
+.pemaPredictorCoefficientNames <- function(predictor, coefficientNames, options){
+  # this functions find coefficients matching to the given predictor
+  # i.e., what are the dummy coefficients for a given factor parameter
+
+  predictors <- sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":"))
+  predictors <- predictors[predictor != predictors]
+
+
+  if (grepl(":", predictor, fixed = TRUE)) {
+
+    # filter out non interactions if an interaction is searched for
+    coefficientNames <- coefficientNames[grepl(":", coefficientNames, perl = TRUE)]
+    predictors       <- predictors[grepl(":", predictors, perl = TRUE)]
+
+    coefficientNamesSplit <- lapply(coefficientNames, function(coefficientName) unlist(strsplit(coefficientName, ":", fixed = TRUE)))
+
+    # remove all coefficients that match other terms
+    indx <- rep(TRUE, length(coefficientNames))
+    for (i in seq_along(predictors)) {
+
+      tempTest <- unlist(strsplit(predictors[i], ":", fixed = TRUE))
+      indx     <- indx & !sapply(coefficientNamesSplit, function(coefficientNameSplit) {
+        all(sapply(tempTest, function(test) any(grepl(test, coefficientNameSplit))))
+      })
+
+    }
+
+    coefficientNames <- coefficientNames[indx]
+    return(coefficientNames)
+
+  } else {
+
+    # filter out interactions if a non interaction is searched for
+    coefficientNames <- coefficientNames[!grepl(":", coefficientNames, perl = TRUE)]
+    predictors       <- predictors[!grepl(":", predictors, perl = TRUE)]
+
+    coefficientNames <- coefficientNames[grepl(predictor, coefficientNames)]
+    return(coefficientNames)
+  }
+}
+.pemaPriors                    <- function(options) {
+  switch(
+    options[["method"]],
+    lasso = c(
+      df    = options[["priorHsDf"]],
+      scale = options[["priorHsScale"]]),
+    hs    = c(
+      df            = options[["priorLassoDf"]],
+      df_global     = options[["priorLassoDfGlobal"]],
+      df_slab       = options[["priorLassoDfSlab"]],
+      scale_global  = options[["priorLassoScaleGlobal"]],
+      scale_slab    = options[["priorLassoScaleSlab"]],
+      relevant_pars = NULL)
+  )
+}
+.pemaFit                       <- function(jaspResults, dataset, options) {
+
+  if (!is.null(jaspResults[["models"]])) {
+    return()
+  } else {
+    model <- createJaspState()
+    model$dependOn(.pemaDependencies)
+    jaspResults[["model"]] <- model
+  }
+
+  saveRDS(list(
+    formula = .pemaFormula(options),
+    dataset = dataset
+  ), file = "C:/JASP/jaspMetaAnalysis/inf.RDS")
+
+  .setSeedJASP(options)
+  # fit <- try(pema::brma(
+  #   formula   = .pemaFormula(options),
+  #   data      = dataset,
+  #   vi        = "JASP_computed_variance__",
+  #   study     = if (options[["clustering]] != "") options[["clustering]],
+  #   method    = options[["method"]],
+  #   prior     = .pemaPriors(options),
+  #   mute_stan = TRUE,
+  #   chains    = options[["mcmcChains"]],
+  #   warmup    = options[["mcmcWarmup"]],
+  #   iter      = options[["mcmcWarmup"]] + options[["mcmcIter"]],
+  #   seed      = if (options[["setSeed]]) .getSeedJASP(options) else sample.int(.Machine$integer.max, 1),
+  #   control   = list(adapt_delta = options[["mcmcDelta"]], max_treedepth = options[["mcmcTreedepth"]])
+  # ))
+  fit <- readRDS(file = "C:/JASP/jaspMetaAnalysis/fit.RDS")
+  #  saveRDS(fit, file = "C:/JASP/jaspMetaAnalysis/fit.RDS")
+
+  model[["object"]] <- fit
+
+  return()
+}
+.pemaSummaryTable              <- function(jaspResults, options) {
+
+  if (!is.null(jaspResults[["summaryTable"]]))
+    return()
+
+  model <- jaspResults[["model"]]$object
+
+  summaryTable <- createJaspTable(title = gettext("Coefficients"))
+  summaryTable$position <- 1
+  summaryTable$dependOn(c(.pemaDependencies, "estimatesCoefficients"))
+
+  summaryTable$addColumnInfo(name = "term",     title = "Term",           type = "string")
+  summaryTable$addColumnInfo(name = "estimate", title = "Estimate",       type = "number")
+  summaryTable$addColumnInfo(name = "se",       title = "SE",             type = "number")
+  summaryTable$addColumnInfo(name = "lowerCI",  title = gettext("Lower"), type = "number", overtitle = gettextf("%s%% CI", 100 * .95))
+  summaryTable$addColumnInfo(name = "upperCI",  title = gettext("Upper"), type = "number", overtitle = gettextf("%s%% CI", 100 * .95))
+  summaryTable$addColumnInfo(name = "rhat",     title = "R-hat",          type = "number")
+  summaryTable$addColumnInfo(name = "neff",     title = "ESS",            type = "number")
+
+  jaspResults[["summaryTable"]] <- summaryTable
+
+  if (is.null(model))
+    return()
+  else if (jaspBase::isTryError(model)) {
+    summaryTable$setError(model)
+    return()
+  }
+
+
+  modelSummary <- model[["coefficients"]]
+  modelSummary <- modelSummary[!rownames(modelSummary) %in% c("tau2", "tau2_w", "tau2_b"),]
+
+  for (i in 1:nrow(modelSummary)) {
+
+    tempRow <- list(
+      term     = .pemaVariableNames(rownames(modelSummary)[i], sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":"))),
+      estimate = modelSummary[i, "mean"],
+      se       = modelSummary[i, "sd"],
+      lowerCI  = modelSummary[i, "2.5%"],
+      upperCI  = modelSummary[i, "97.5%"],
+      rhat     = modelSummary[i, "Rhat"],
+      neff     = modelSummary[i, "n_eff"]
+    )
+
+    summaryTable$addRows(tempRow)
+  }
+
+  return()
+}
+.pemaSummaryTableTau           <- function(jaspResults, options) {
+
+  if (!is.null(jaspResults[["summaryTauTable"]]))
+    return()
+
+  model <- jaspResults[["model"]]$object
+
+  summaryTauTable <- createJaspTable(title = gettext("Heterogeneity"))
+  summaryTauTable$position <- 2
+  summaryTauTable$dependOn(c(.pemaDependencies, "estimatesTau"))
+
+  summaryTauTable$addColumnInfo(name = "term",     title = "Term",           type = "string")
+  summaryTauTable$addColumnInfo(name = "estimate", title = "Estimate",       type = "number")
+  summaryTauTable$addColumnInfo(name = "lowerCI",  title = gettext("Lower"), type = "number", overtitle = gettextf("%s%% CI", 100 * .95))
+  summaryTauTable$addColumnInfo(name = "upperCI",  title = gettext("Upper"), type = "number", overtitle = gettextf("%s%% CI", 100 * .95))
+
+  jaspResults[["summaryTauTable"]] <- summaryTauTable
+
+  if (is.null(model))
+    return()
+  else if (jaspBase::isTryError(model)) {
+    summaryTable$setError(model)
+    return()
+  }
+
+  modelSamples <- rstan::extract(model$fit)
+
+  if (options[["clustering"]] == "") {
+
+    tau2Samples  <- modelSamples$tau2
+
+    summaryTauTable$addRows(list(
+      term     = "\U1D6D5\U00B2",
+      estimate = mean(tau2Samples),
+      lowerCI  = quantile(tau2Samples, probs = .025),
+      upperCI  = quantile(tau2Samples, probs = .975)
+    ))
+
+    summaryTauTable$addRows(list(
+      term     = "\U1D6D5",
+      estimate = mean(sqrt(tau2Samples)),
+      lowerCI  = quantile(sqrt(tau2Samples), probs = .025),
+      upperCI  = quantile(sqrt(tau2Samples), probs = .975)
+    ))
+
+  } else {
+
+    tau2WSamples  <- modelSamples$tau2_w
+    tau2BSamples  <- modelSamples$tau2_b
+
+    summaryTauTable$addRows(list(
+      term     = "\U1D6D5\U00B2 (within)",
+      estimate = mean(tau2WSamples),
+      lowerCI  = quantile(tau2WSamples, probs = .025),
+      upperCI  = quantile(tau2WSamples, probs = .975)
+    ))
+    summaryTauTable$addRows(list(
+      term     = "\U1D6D5\U00B2 (between)",
+      estimate = mean(tau2BSamples),
+      lowerCI  = quantile(tau2BSamples, probs = .025),
+      upperCI  = quantile(tau2BSamples, probs = .975)
+    ))
+
+    summaryTauTable$addRows(list(
+      term     = "\U1D6D5 (within)",
+      estimate = mean(sqrt(tau2WSamples)),
+      lowerCI  = quantile(sqrt(tau2WSamples), probs = .025),
+      upperCI  = quantile(sqrt(tau2WSamples), probs = .975)
+    ))
+    summaryTauTable$addRows(list(
+      term     = "\U1D6D5 (between)",
+      estimate = mean(sqrt(tau2BSamples)),
+      lowerCI  = quantile(sqrt(tau2BSamples), probs = .025),
+      upperCI  = quantile(sqrt(tau2BSamples), probs = .975)
+    ))
+
+  }
+
+
+  return()
+}
+.pemaDiagnostics               <- function(jaspResults, options) {
+
+  if (!is.null(jaspResults[["diagnosticPlots"]]))
+    return()
+
+  diagnosticPlots <- createJaspContainer(title = gettext("Sampling diagnostics"))
+  diagnosticPlots$position <- 4
+  diagnosticPlots$dependOn(c(.pemaDependencies, "diagnosticsType", "diagnosticsVariable1", "diagnosticsVariable2"))
+  jaspResults[["diagnosticPlots"]] <- diagnosticPlots
+
+  model <- jaspResults[["model"]]$object
+
+  if ((options$samplingPlot == "stan_scat" && length(options[["diagnosticsVariable2"]]) == 0) || is.null(model) || jaspBase::isTryError(model)) {
+    diagnosticPlots[["emptyPlot"]] <- createJaspPlot()
+    return()
+  }
+
+  stanFit          <- pema::as.stan(model)
+  stanSamples      <- rstan::extract(stanFit)
+  coefficientNames <- rownames(model$coefficients)
+
+  if (options[["diagnosticsType"]] == "stan_scat") {
+
+    coefficients1 <- .pemaPredictorCoefficientNames(paste0(options[["diagnosticsVariable1"]][[1]]$variable, collapse = ":"), coefficientNames, options)
+    coefficients2 <- .pemaPredictorCoefficientNames(paste0(options[["diagnosticsVariable2"]][[1]]$variable, collapse = ":"), coefficientNames, options)
+
+    for(i in seq_along(coefficients1)){
+      for(j in seq_along(coefficients2)){
+
+        tempPlot <- rstan::stan_scat(object = stanFit, pars = c(coefficients1[i], coefficients2[j]))
+
+        xTicks <- jaspGraphs::getPrettyAxisBreaks(range(stanSamples[[coefficients1[i]]]))
+        yTicks <- jaspGraphs::getPrettyAxisBreaks(range(stanSamples[[coefficients2[j]]]))
+
+        tempPlot <- tempPlot +
+          ggplot2::scale_x_continuous(.pemaVariableNames(
+            coefficients1[i],
+            sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":"))),
+            breaks = xTicks,
+            limits = range(xTicks)) +
+          ggplot2::scale_y_continuous(.pemaVariableNames(
+            coefficients2[j],
+            sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":"))),
+            breaks = yTicks,
+            limits = range(yTicks))
+        tempPlot <- tempPlot + jaspGraphs::geom_rangeframe(sides = "bl") + jaspGraphs::themeJaspRaw()
+
+        tempJaspPlot            <- createJaspPlot(title = "", width = 400, height = 300)
+        tempJaspPlot$plotObject <- tempPlot
+
+        diagnosticPlots[[paste0("diagnostics", "_", i, "_", j)]] <- tempJaspPlot
+      }
+    }
+
+  } else {
+
+    coefficients <- .pemaPredictorCoefficientNames(paste0(options[["diagnosticsVariable1"]][[1]]$variable, collapse = ":"), coefficientNames, options)
+
+    for(i in seq_along(coefficients)){
+
+      tempPlot <- switch(
+        options[["diagnosticsType"]],
+        "stan_trace" = rstan::traceplot(stanFit, coefficients[i]),
+        "stan_hist"  = rstan::stan_hist(stanFit, coefficients[i]),
+        "stan_dens"  = rstan::stan_dens(stanFit, coefficients[i], separate_chains = TRUE),
+        "stan_ac"    = rstan::stan_ac(stanFit, coefficients[i])
+      )
+
+      parTicks <- jaspGraphs::getPrettyAxisBreaks(range(stanSamples[[coefficients[i]]]))
+
+      if (options[["diagnosticsType"]] %in% c("stan_hist", "stan_dens")) {
+        tempPlot <- tempPlot +
+          ggplot2::scale_x_continuous(.pemaVariableNames(
+            coefficients[i],
+            sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":"))),
+            breaks = parTicks,
+            limits = range(parTicks))
+        tempPlot <- tempPlot + jaspGraphs::geom_rangeframe(sides = "b") + jaspGraphs::themeJaspRaw() +
+          ggplot2::theme(
+            axis.title.y = ggplot2::element_blank(),
+            axis.text.y  = ggplot2::element_blank(),
+            axis.ticks.y = ggplot2::element_blank()
+          )
+      } else if (options[["diagnosticsType"]] %in% c("stan_trace")) {
+        tempPlot <- tempPlot +
+          ggplot2::scale_y_continuous(.pemaVariableNames(
+            coefficients[i],
+            sapply(options[["modelTerms"]], function(term) paste0(term[[1]], collapse = ":"))),
+            breaks = parTicks,
+            limits = range(parTicks))
+        tempPlot <- tempPlot + jaspGraphs::geom_rangeframe(sides = "bl") + jaspGraphs::themeJaspRaw() +
+          ggplot2::theme(plot.margin = ggplot2::margin(r = 10 * (nchar(options[["mcmcWarmup"]] + options[["mcmcIter"]]) - 2)))
+      }
+
+      tempJaspPlot            <- createJaspPlot(title = "", width = 400, height = 300)
+      tempJaspPlot$plotObject <- tempPlot
+
+      diagnosticPlots[[paste0("diagnostics", "_", i)]] <- tempJaspPlot
+    }
+  }
+
+  return()
+}
---
+++ inst/qml/PenalizedMetaAnalysis.qml
@@ -0,0 +1,342 @@
+//
+// Copyright (C) 2013-2018 University of Amsterdam
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+import QtQuick			2.8
+import QtQuick.Layouts	1.3
+import JASP.Controls	1.0
+import JASP.Widgets		1.0
+import JASP				1.0
+import "../qml/qml_components" as MA
+
+Form
+{
+	VariablesForm
+	{
+		preferredHeight: 400 * preferencesModel.uiScale
+
+		AvailableVariablesList
+		{
+			name: "allVariables"
+		}
+
+		AssignedVariablesList
+		{
+			name:				"effectSize"
+			title:				qsTr("Effect Size")
+			singleVariable:		true
+			suggestedColumns:	["scale"]
+		}
+
+		AssignedVariablesList
+		{
+			name:				"standardError"
+			title:				qsTr("Effect Size Standard Error")
+			singleVariable:		true
+			suggestedColumns:	["scale"]
+		}
+
+		DropDown
+		{
+			name:			"method"
+			id:				method
+			label:			qsTr("Method")
+			currentIndex:	2
+
+			values: [
+				{ label: qsTr("Horseshoe"),	value: "hs"		},
+				{ label: qsTr("Lasso"),		value: "lasso"	}
+			]
+		}
+
+/*		AssignedVariablesList
+		{
+			name:				"studyLabels"
+			title:				qsTr("Study Labels")
+			singleVariable:		true
+			suggestedColumns:	["ordinal", "nominal"]
+		}
+*/
+		AssignedVariablesList
+		{
+			name:				"covariates"
+			title:				qsTr("Covariates")
+			suggestedColumns:	["scale"]
+		}
+
+		AssignedVariablesList
+		{
+			name:				"factors"
+			title:				qsTr("Factors")
+			suggestedColumns:	["ordinal", "nominal"]
+		}
+
+		AssignedVariablesList
+		{
+			name:				"clustering"
+			title:				qsTr("Clustering")
+			singleVariable:		true
+			suggestedColumns:	["ordinal", "nominal"]
+		}	
+	}
+
+	Section
+	{
+		title: qsTr("Model")
+
+		VariablesForm
+		{
+			preferredHeight: 150 * preferencesModel.uiScale
+
+			AvailableVariablesList
+			{
+				name:		"components"
+				title:		qsTr("Components")
+				source:		["covariates","factors"]
+			}
+
+			AssignedVariablesList
+			{
+				name:			"modelTerms"
+				title:			qsTr("Model Terms")
+				listViewType:	JASP.Interaction
+			}
+		}
+
+		CheckBox
+		{
+			name:		"includeConstant"
+			label:		qsTr("Include intercept")
+			checked:	true
+		}
+	}
+
+	Section
+	{
+		title: qsTr("Priors")
+
+		Group
+		{
+			title:		qsTr("Horseshoe")
+			visible:	method.currentValue == "hs"
+			
+			DoubleField
+			{
+				name:			"priorHsDf"
+				label:			qsTr("Df")
+				defaultValue:	1
+				min:			1
+				inclusive:		JASP.MinOnly
+			}
+
+			DoubleField
+			{
+				name:			"priorHsScale"
+				label:			qsTr("Scale")
+				defaultValue:	1
+				min:			0
+				inclusive:		JASP.None
+			}
+		}
+
+		Group
+		{
+			title:		qsTr("Lasso")
+			visible:	method.currentValue == "lasso"
+			
+			DoubleField
+			{
+				name:			"priorLassoDf"
+				label:			qsTr("Df")
+				defaultValue:	1
+				min:			1
+				inclusive:		JASP.MinOnly
+			}
+
+			DoubleField
+			{
+				name:			"priorLassoDfGlobal"
+				label:			qsTr("Df (global)")
+				defaultValue:	1
+				min:			1
+				inclusive:		JASP.MinOnly
+			}
+
+			DoubleField
+			{
+				name:			"priorLassoDfSlab"
+				label:			qsTr("Df (slab)")
+				defaultValue:	4
+				min:			1
+				inclusive:		JASP.MinOnly
+			}
+
+			DoubleField
+			{
+				name:			"priorLassoScaleGlobal"
+				label:			qsTr("Scale (global)")
+				defaultValue:	1
+				min:			0
+				inclusive:		JASP.None
+			}
+
+			DoubleField
+			{
+				name:			"priorLassoScaleSlab"
+				label:			qsTr("Scale (slab)")
+				defaultValue:	1
+				min:			0
+				inclusive:		JASP.None
+			}
+		}
+	}
+
+	Section
+	{
+		title:	qsTr("Inference")
+
+		Group
+		{
+			CheckBox
+			{
+				name:		"estimatesCoefficients"
+				label:		qsTr("Estimates")
+				checked:	true
+			}
+
+			CheckBox
+			{
+				name:		"estimatesTau"
+				label:		qsTr("Heterogeneity")
+				checked:	false
+			}
+		}
+	}
+
+
+	Section
+	{
+		title:		qsTr("MCMC diagnostics")
+
+		VariablesForm
+		{
+			preferredHeight: 200 * preferencesModel.uiScale
+
+			AvailableVariablesList
+			{
+				name:	"diagnosticsVariables"
+				title:	qsTr("Model terms")
+				source:	"modelTerms"
+			}
+
+			AssignedVariablesList
+			{
+				singleVariable:	true
+				name:			"diagnosticsVariable1"
+				title:			samplingPlot.currentValue == "stan_scat" ? qsTr("Horizontal axis") : qsTr("Plotted term")
+			}
+
+			AssignedVariablesList
+			{
+				singleVariable:	true
+				name:			"diagnosticsVariable2"
+				title:			qsTr("Vertical axis")
+				visible:		samplingPlot.currentValue == "stan_scat"
+				onVisibleChanged: if (!visible && count > 0) itemDoubleClicked(0)
+			}
+		}
+
+		DropDown
+		{
+			name:	"diagnosticsType"
+			id:		samplingPlot
+			label:	qsTr("Plot type")
+			values:
+			[
+				{ label: qsTr("Traceplot"),			value: "stan_trace"},
+				{ label: qsTr("Scatterplot"),		value: "stan_scat"},
+				{ label: qsTr("Histogram"),			value: "stan_hist"},
+				{ label: qsTr("Density"),			value: "stan_dens"},
+				{ label: qsTr("Autocorrelations"),	value: "stan_ac"}
+			]
+		}
+	}
+
+
+	Section
+	{
+		columns: 	2
+		title: 		qsTr("Advanced")
+		
+		Group
+		{
+			title: 		qsTr("Estimation settings (MCMC)")
+			columns: 	1
+
+			IntegerField
+			{
+				label: 			qsTr("Warmup")
+				name: 			"mcmcWarmup"
+				defaultValue: 	2000
+				min:			100
+				max: 			1000000
+				fieldWidth: 	100
+			}
+
+			IntegerField
+			{
+				label: 			qsTr("Iterations")
+				name: 			"mcmcIter"
+				defaultValue: 	2000
+				min:			100
+				max: 			1000000
+				fieldWidth: 	100
+			}
+
+			IntegerField
+			{
+				label: 			qsTr("Chains")
+				name: 			"mcmcChains"
+				defaultValue: 	4
+				min:			1
+				max: 			10
+				fieldWidth: 	50
+			}
+
+			DoubleField
+			{
+				label: 			qsTr("Adapt delta")
+				name: 			"mcmcDelta"
+				defaultValue: 	0.80
+				min:			0.05
+				max: 			1
+				fieldWidth: 	50
+			}
+
+			IntegerField
+			{
+				label: 			qsTr("Maximum treedepth")
+				name: 			"mcmcTreedepth"
+				defaultValue: 	15
+				min:			5
+				max: 			100
+				fieldWidth: 	50
+			}
+		}
+
+		SetSeed { }
+	}
+}
